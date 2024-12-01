1d0a1afe4e734ee7b65cdee61983e7720b5e0711
Alix Leve <alix.leve@gmail.com>
2024-12-01 15:01:22 +0100

    add: functionnalities for programm

--- day1.py
+++ day1.py
@@ -1,23 +1,31 @@
 list1 = [3,4,2,1,3,3]
 list2 = [4,3,5,3,9,3]
-list_of_distance = []
+
+#TO DO : revoir ordre exécution fonction, quand il n'y a plus que 2 éléments dans chaque list
+#distance pas calculée pour quand 2 éléments par liste !!
 
 def main():
-    result1 = smallest_number(list1)
-    print("smaller element n-1: ", result1)
-    result2 = smallest_number(list2)
-    print("smaller element n-2: ", result2)
-    distance = calcul_distance(result1, result2)
-    print("distance: ", distance)
+    list_of_distance = compare_list(list1, list2)
+    total_distance = sum_of_distance(list_of_distance)
+    print("total of distance: ", total_distance)
+
 
 
 def smallest_number(list):
     smaller_element = list[0]
-    print("smaller element: " , smaller_element)
+
     for element in list:
         if element < smaller_element:
-            smaller_element = element
-            print("new smaller element: ", smaller_element)      
+            smaller_element = element 
+    
+    if len(list) == 2:
+        print("DANS LE IF")
+        for element in list:
+            if list[0] > list[1]:
+                return list[1]
+            else:
+                return list[0]
+
     return smaller_element
 
 
@@ -28,14 +36,53 @@ def calcul_distance(num1, num2):
         distance = num2 - num1    
     return distance
 
+
 def remove_smallest_number(number, list):
-    for element in list:
-        if element == number:
-            list.remove(number)
-    print("list: ", list) 
+    list.remove(number)
     return list  
      
+def compare_list(list1, list2):
+    #appliquer sur chaque élément de list1 smallest_number
+    #renvoie un résultat
+    new_list1 = list1
+    new_list2 = list2
+    listing_distance = []
 
+    while len(new_list1) > 1:
+        while len(new_list2) >= 1:
+            result1 = smallest_number(new_list1)
+            print("smaller element n-1: ", result1)
+            result2 = smallest_number(list2)
+            print("smaller element n-2: ", result2)
+            new_list1 = remove_smallest_number(result1, new_list1)
+            print("new list 1: ", new_list1) 
+            new_list2 = remove_smallest_number(result2, new_list2)
+            print("new list 2: ", new_list2) 
+
+            if len(new_list1) == 1:
+                result1 = new_list1[0]
+                print("smaller element n-1: ", result1)
+
+            if len(new_list2) == 1:
+                result2 = new_list2[0]
+                print("smaller element n-2: ", result2)
+            
+    #appliquer calcul_distance pour chaque couple de résultat
+            distance = calcul_distance(result1, result2)
+            print("distance: ", distance)
+
+    #stocker ces résultats dans une nouvelle liste
+            listing_distance.append(distance)
+            print("list of distance: ", listing_distance)
+
+    return listing_distance
+
+
+def sum_of_distance(list):
+    sum = 0
+    for element in list:
+        sum += element
+    return sum
 
 main()
 
@@ -52,7 +99,3 @@ main()
 # Finally, the largest number in the left list is 4, while the largest number in the right list is 9; these are a distance 5 apart.
 # To find the total distance between the left list and the right list, add up the distances between all of the pairs you found. 
 # In the example above, this is 2 + 1 + 0 + 1 + 2 + 5, a total distance of 11!
-
-# appliquer sur chaque élément de la liste la fonction smallest
-# comparer le résultat à l'aide de calculdistance
-# comparer ces deux plus petits éléments pour trouver la distance entre eux deux
