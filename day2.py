b5b75f17a6c560ead49348be540178d798d300b3
Alix Levé <alix.leve@gmail.com>
2024-12-12 17:11:49 +0100

    effective  counter, refacto has to be done

--- day2.py
+++ day2.py
@@ -1,10 +1,25 @@
 sample = [[7, 6, 4, 2, 1], [1, 2, 7, 8, 9], [9, 7, 6, 2, 1], [1, 3, 2, 4, 5], [8, 6, 4, 4, 1], [1, 3, 6, 7, 9]]
-counter = 0
+
 
 def main():
     reports = readFile()
-    newCounter = checkDecrease(sample, counter)
-    print("NEW COUNTER: ", newCounter)
+    counter = 0
+    for list in reports:
+        print(list)
+        result_decrease = checkDecrease(list)
+        if result_decrease == "safe":
+                result_decrease_adjacent = checkDecreaseAdjacent(list) 
+                print(f"🌺 {result_decrease_adjacent}")
+                if result_decrease_adjacent == "safe":
+                    counter += 1            
+        else:
+            result_increase = checkIncrease(list)
+            if result_increase == "safe":
+                result_increase_adjacent = checkIncreaseAdjacent(list)
+                print(f"💐 {result_increase_adjacent}")
+                if result_increase_adjacent == "safe":
+                    counter +=1
+    print(f"🌕 {counter}")                
 
 
 def readFile():
@@ -16,28 +31,22 @@ def readFile():
                 line = [int(i) for i in line]
                 report.append(line)      
     return report
-
-
-def checkDecrease(lists, count):
-    res = "safe"
-    for list in lists:
-        for i in range(1, len(list)):
-            if list[i] >= list[i -1]:
-                res = "unsafe"
-                increaseCounter = checkIncrease(list, counter)
-                print(f"🌴 {increaseCounter}")
-                
-                
-        if res == "safe":        
-            newCounter = checkDecreaseAdjacent(list, counter) 
-            print(f"🌿 {newCounter}")   
-            count += newCounter
     
-    count += increaseCounter
-    return count         
+
+def checkDecrease(list):
+    res = "safe"
+    for i in range(1, len(list)):
+        if list[i] >= list[i -1]:
+            print(list[i] >= list[i -1])
+            res = "unsafe"
+            
+        if res == "safe":   
+            print(f"🌹 safe")    
+    
+    return res        
 
 
-def checkIncrease(list, count):
+def checkIncrease(list):
     res = "safe"
     for i in range(1, len(list)):
         if list[i] <= list[i -1]:
@@ -45,14 +54,11 @@ def checkIncrease(list, count):
 
     if res == "safe":
         print(f"🐚 safe : {list}")
-        newCounter = checkIncreaseAdjacent(list, counter) 
-        print(f"🍄‍🟫 {newCounter}")  
-        count += newCounter
-    
-    return count     
+      
+    return res     
        
             
-def checkDecreaseAdjacent(list, count):
+def checkDecreaseAdjacent(list):
     res = "safe"
     for i in range(1, len(list)):
         if list[i -1] - list[i] < 1 or list[i -1] - list[i] > 3:
@@ -60,24 +66,20 @@ def checkDecreaseAdjacent(list, count):
          
     if res == "safe": 
         print("🍏safe: ", list)        
-        count += 1
-        
-    print(f"🐓 {count}")
-    return count                         
+
+    return res                      
 
 
-def checkIncreaseAdjacent(list, count):
+def checkIncreaseAdjacent(list):
     res = "safe"
     for i in range(1, len(list)):
         if list[i] - list[i -1] < 1 or list[i] - list[i -1] > 3:
             res = "unsafe"
             
     if res == "safe": 
-        print("🍓safe: ", list)       
-        count += 1
-        
-    print(f"🌻 {count}")
-    return count    
+        print(f"🐪 safe")
+
+    return res  
              
 
 main()
